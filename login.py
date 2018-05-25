59d77ecb9d00670450fc781dcc07b5b97e6fc606
mark <zhangyj_mark@126.com>
2018-05-25 22:31:19 +0800

    dev1_first

--- login.py
+++ login.py
@@ -29,3 +29,25 @@ num3 = 30
 num4 = 40
 num5 = 50
 num6 = 60
+num7 = 60
+
+from flask import Flask
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+
+
+app = Flask(__name__)
+manager = Manager(app)
+manager.add_command('mysql', MigrateCommand)
+
+
+
+@app.route("/index")
+def index():
+    return "index"
+
+
+
+
+if __name__ == '__main__':
+    manager.run()
