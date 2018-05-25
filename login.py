48767b453de22af59a3f4c496fbcc8013c86daa6
mark <zhangyj_mark@126.com>
2018-05-25 22:14:52 +0800

    num7

--- login.py
+++ login.py
@@ -1,3 +1,28 @@
+from flask import Flask
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+
+
+app = Flask(__name__)
+manager = Manager(app)
+manager.add_command('mysql', MigrateCommand)
+
+
+
+@app.route("/index")
+def index():
+    return "index"
+
+
+
+
+if __name__ == '__main__':
+    manager.run()
+
+
+
+
+
 num1 = 10
 num2 = 20
 num3 = 30
